0ac39a109751c524399d3e8603c678ace2d2cb5d
louisChiffre <>
2024-09-29 10:18:11 +0200

    Add more explanations in processing.py

--- variance/processing.py
+++ variance/processing.py
@@ -14,6 +14,8 @@ from intervaltree import Interval, IntervalTree
 from variance.medite.utils import pretty_print, make_html_output
 import re
 import itertools
+import logging 
+logger = logging.getLogger(__name__)
 
 
 namespaces = {"": "http://www.tei-c.org/ns/1.0"}
@@ -151,25 +153,29 @@ def xml2txt(filepath: pathlib.Path) -> Output:
     """extract text from xml and apply pre-processing step to text"""
     soup = read(filepath=filepath)
 
-    doc = {}
+    # we keep track where each paragraph
+    # 0-51   -- paragraph 1
+    # 52-108 -- paragramp 2
+    # ...
+    # this will be important when we transform back the medite output to tei
     tree = IntervalTree()
-    tree_txt = IntervalTree()
+    
     # Find all <p> elements
     body = soup.find("body")
-
     # Add unique IDs to each element
     for i, element in enumerate(soup.find_all("p")):
         element["id"] = f"#{i}"
     esc = add_escape_characters
 
+    # go through p elements in the body apply the transformations and return texts
     def gen():
         cursor = 0
         for div in body.find_all("div"):
             p_elements = div.find_all("p")
-            # breakpoint()
             for p in p_elements:
-
+                # for each paragraph, we construct the text
                 def gen_p():
+                    # if there 
                     for content in p.contents:
                         if content.name == "emph":
                             # TODO verify there is no escape character to be done here
@@ -178,24 +184,25 @@ def xml2txt(filepath: pathlib.Path) -> Output:
                             yield content
                         elif content.name is None and content.string:
                             yield content.string
-                    # yield newline
 
-                # txt = "".join([add_escape_characters(k) for k in gen_p()])
                 txt_ = "".join(gen_p())
+                # then apply the transformations
                 txt = esc(txt_)
+                # and add a newline
                 txt = txt + newline
-                # compare(txt_ + newline, txt)
 
+                # we then update the mapping character range -> paragraph
                 old_cursor, cursor = cursor, cursor + len(txt)
+                # we need to keep track
                 tree[old_cursor:cursor] = p
                 yield txt
 
     txts = list(gen())
-    # breakpoint()
     txt = "".join(txts)
 
+    # we store the txt file in txt
     txt_filepath = filepath.with_suffix(".txt")
-    print(f"printing pre-process {txt_filepath}")
+    logger.info(f"printing pre-process {txt_filepath}")
     txt_filepath.write_text(txt, encoding="utf-8")
 
     # ps = [k.data for k in sorted(tree, key=lambda x: x.begin)]
@@ -216,6 +223,7 @@ def xml2txt(filepath: pathlib.Path) -> Output:
     # n_p_ = len(txt.split(newline)[:-1])
     # assert n_p == n_p_
 
+    # the output of the function contains the txt, but also the original xml document and the character to paragraph mapping
     return Output(
         id=soup.find("TEI")["xml:id"], txt=txt, soup=soup, tree=tree, path=filepath
     )
@@ -235,10 +243,12 @@ R = namedtuple("R", "a_start a_end b_start b_end")
 def calc_revisions(z1: Output, z2: Output, parameters: md.Parameters) -> Result:
     """apply medite on the two texts and generate pairs of modifications"""
 
+    # we call medite
+    appli = md.DiffTexts(chaine1=z1.txt, chaine2=z2.txt, parameters=parameters)
+
+    # we then retrieve the detlas in a structure that will allow us to construct the TEI xml
     def t2n(x):
         return [Block(*k) for k in x]
-
-    appli = md.DiffTexts(chaine1=z1.txt, chaine2=z2.txt, parameters=parameters)
     N = len(z1.txt)
 
     def handle(x):
@@ -253,6 +263,7 @@ def calc_revisions(z1: Output, z2: Output, parameters: md.Parameters) -> Result:
                 return R(a_start, a_end, b_start - N, b_end - N)
             # TODO clarify the meaning of R with pair of number at the end
             # it seems it is for the case when a block was moved and this block replace an existing block
+            # it's a Deplacement/Replacement
             # A ---+
             #      |
             #      v
@@ -268,7 +279,8 @@ def calc_revisions(z1: Output, z2: Output, parameters: md.Parameters) -> Result:
                 raise Exception(f"cannot match {x}")
 
     deltas = [handle(k) for k in appli.bbl.liste]
-    # we verify nothing was lost
+    
+    # we verify we can reconstruct the two texts from the deltas
 
     # we reconstruct the first text
     z = [k for k in deltas if isinstance(k, (BC, S, R, DA))]
@@ -283,12 +295,13 @@ def calc_revisions(z1: Output, z2: Output, parameters: md.Parameters) -> Result:
         yield from [(k.b_start, k.b_end) for k in deltas if isinstance(k, (BC, R))]
 
     txt2 = "".join([z2.txt[k[0] : k[1]] for k in sorted(gen())])
-    act = txt2
-    ref = z2.txt
-    k = next((i for i, z in enumerate(zip(act, ref)) if z[0] != z[1]), None)
-    assert k is None
+    # tidbit to facilitate debugging, will flag the first character that has changed
+    # act = txt2
+    # ref = z2.txt
+    # k = next((i for i, z in enumerate(zip(act, ref)) if z[0] != z[1]), None)
+    # assert k is None
+
     assert txt2 == z2.txt
-    # assert len(txt2)==len(z2.txt)
     return Result(appli=appli, deltas=deltas)
 
 
@@ -299,10 +312,11 @@ def process(
     output_filepath: pathlib.Path,
 ):
     """the main function"""
+    # we transform the xml in text with medite annotations
     z1 = xml2txt(source_filepath)
     z2 = xml2txt(target_filepath)
 
-    # let's generte the xml
+    # create the skeleton of the xml
     root = ET.Element(
         "{http://www.tei-c.org/ns/1.0}TEI",
         {
@@ -341,23 +355,32 @@ def process(
         "substitution": ET.SubElement(medite_data, "listSubstitution"),
     }
 
+
+    # execute medite
+    res = calc_revisions(z1=z1, z2=z2, parameters=parameters)
+
+    # we create the html for debugging/verification purpose purpose
+    make_html_output(
+        appli=res.appli, html_filename=output_filepath.with_suffix(".html")
+    )
+
+    # populate the xml
+    updated = set()
+
+    # there is a series of utility functions
     def add_list(txt, attributes, name):
+        '''add change to list of change for the list tags of mediteData'''
         list_elem = lists[name]
         elem = ET.SubElement(lists[name], name, attributes)
         if txt:
             elem.text = remove_medite_annotations(txt)
 
-    res = calc_revisions(z1=z1, z2=z2, parameters=parameters)
-    make_html_output(
-        appli=res.appli, html_filename=output_filepath.with_suffix(".html")
-    )
-
-    updated = set()
-
     def metamark(function: str, target: str):
+        '''creates a metamark'''
         return z1.soup.new_tag("metamark", function=function, target=target)
 
     def zip_paragraphs(start: int, end: int):
+        '''given a charactr range, returns the associated paragraphs and texts'''
         txt = z1.txt[start:end]
         # para_txts_ = [k for k in txt.split(newline)]
 
@@ -369,17 +392,17 @@ def process(
 
         para_htms = sorted(z1.tree[start:end], key=lambda x: x.begin)
         ids = [k.data["id"] for k in para_htms]
-        print(f"paragraphs between {start} end {end}".center(80, "#"))
-        print(f"text: [{txt}]")
+        logger.debug(f"paragraphs between {start} end {end}".center(80, "#"))
+        logger.debug(f"text: [{txt}]")
         for i, x in enumerate(zip_longest(para_txts, para_htms)):
             t, h = x
-            print(f"paragraph {i}".center(80, "*"))
-            print(f"text:\n[{t}]\n------\n")
+            logger.debug(f"paragraph {i}".center(80, "*"))
+            logger.debug(f"text:\n[{t}]\n------\n")
             if h is not None:
-                print(f"html:\n{h.data}\n------\n")
+                logger.debug(f"html:\n{h.data}\n------\n")
             else:
-                print(f"html:\n{h}\n------\n")
-        print(f"end paragraph".center(80, "#"))
+                logger.debug(f"html:\n{h}\n------\n")
+        logger.debug(f"end paragraph".center(80, "#"))
         assert len(para_txts) >= len(para_htms)
         assert len(para_htms) > 0
         # breakpoint()
@@ -410,7 +433,11 @@ def process(
         elif isinstance(z, DB):
             key = z2.txt[z.start : z.end]
             z2_moved_blocks[key] = z
+
+
     # we have to fill out the moved blocks that are part of a replacement
+    # not implemented yet as we need clarification
+    # TODO clarify and implement
     for key in set(z1_moved_blocks).difference(z2_moved_blocks):
         # r_block = next((k for k in res.deltas if isinstance(k,R) if z2.txt[k.b_start:k.b_end]==key))
         # z2_moved_block[key] =
@@ -419,11 +446,13 @@ def process(
     for key in set(z2_moved_blocks).difference(z1_moved_blocks):
         pass
         # breakpoint()
-
     # we verify that for every moved block in z1 there is a coresponding block in z2
+    # currently not active as the TODO above is not implemented
     # assert set(z1_moved_blocks) == set(z2_moved_blocks)
 
     def append_text(tag, start: int, end: int):
+        '''create xml data for a character range of the text'''
+        # character range can cross several paragraphs
         for i, P in enumerate(zip_paragraphs(start=start, end=end)):
             id, paragraph, txt = P
             zp = paragraph.data
@@ -436,9 +465,11 @@ def process(
             # breakpoint()
             zp.append(txt)
 
+    # let's go through the deltas
     for z in res.deltas:
+        # each type of change require a different handling
         if isinstance(z, BC):
-            print("BLOC COMMUN".center(120, "$"))
+            logger.debug("BLOC COMMUN".center(120, "$"))
             id_v1 = f"v1_{z.a_start}_{z.a_end}"
             id_v2 = f"v2_{z.b_start}_{z.b_end}"
             tag = z1.soup.new_tag(
@@ -446,10 +477,9 @@ def process(
             )
             append_text(tag=tag, start=z.a_start, end=z.a_end)
         elif isinstance(z, S):
-            print("SUPPRESION".center(120, "$"))
+            logger.debug("SUPPRESION".center(120, "$"))
             target_id = f"v1_{z.start}_{z.end}"
             tag = metamark(function="del", target=target_id)
-
             append_text(tag=tag, start=z.start, end=z.end)
             txt = z1.txt[z.start : z.end]
             if txt.strip() == "":
@@ -462,7 +492,7 @@ def process(
                 )
 
         elif isinstance(z, I):
-            print("INSERTION".center(120, "$"))
+            logger.debug("INSERTION".center(120, "$"))
             target_id = f"v2_{z.start}_{z.end}"
             tag = metamark(function="ins", target=target_id)
             current_paragraph = paragraph_stack[-1]
@@ -475,7 +505,7 @@ def process(
             )
 
         elif isinstance(z, DA):
-            print("MOVE A".center(120, "$"))
+            logger.debug("MOVE A".center(120, "$"))
             # breakpoint()
             key = z1.txt[z.start : z.end]
             # we retrieve the corresponding block in the second text
@@ -494,10 +524,7 @@ def process(
                 txt=key, attributes=dict(target=id_v1, corresp=id_v2), name="transpose"
             )
         elif isinstance(z, DB):
-            print("MOVE B".center(120, "$"))
-            # breakpoint()
-            # key = z2.txt[z.start:z.end]
-
+            logger.debug("MOVE B".center(120, "$"))
         elif isinstance(z, R):
             id_v1 = f"v1_{z.a_start}_{z.a_end}"
             id_v2 = f"v2_{z.b_start}_{z.b_end}"
@@ -513,7 +540,7 @@ def process(
         else:
             raise NotImplementedError(f"Element of type {z} is not supported")
 
-    # remove the ids
+    # let's do some cleanups
     for element in z1.soup.find_all():
         if "id" in element.attrs and element["id"].startswith("#"):
             del element["id"]
@@ -548,12 +575,9 @@ def process(
     s2 = to_txt(output_filepath)
     assert len(s1) > 0
     result = testfixtures.compare(
-        s1, s2, x_label="original text", y_label="processed text", raises=False
+        s1, s2, x_label="original text", y_label="processed text", raises=True
     )
-    if not result:
-        print("Comparison failed!")
-    else:
-        print("Comparison succeeded!")
+
 
 
 def create_tei_xml(
