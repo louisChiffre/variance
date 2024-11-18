e17e5f8dc64b80cebc9f0cad1f5607825f052c5b
louisChiffre <>
2024-11-18 06:43:28 +0100

    Add version number argument to txt2tei script

--- scripts/txt2tei.py
+++ scripts/txt2tei.py
@@ -13,23 +13,24 @@ logging.basicConfig(
 @click.argument("source_filename", type=click.Path(exists=True))
 @click.option("--pub_date_str", default="unknown", help="Publication date string")
 @click.option("--title", default="unknown", help="Title string")
+@click.option("--version_nb", default=1, help="Version number")
 def run(
     source_filename,
     pub_date_str,
-    title
+    title,
+    version_nb
 ):
     source_filepath = pathlib.Path(source_filename)
     if source_filepath.suffix != ".txt":
         raise ValueError('source file must be a ".txt" file')
 
-
-    logger.info(f"creating TEI file from {source_filepath} with pub_date_str={pub_date_str} and title={title}")
+    logger.info(f"creating TEI file from {source_filepath} with pub_date_str={pub_date_str}, title={title}, and version_nb={version_nb}")
 
     target_filepath = p.create_tei_xml(
         path=source_filepath,
         pub_date_str=pub_date_str,
         title_str=title,
-        version_nb=2,
+        version_nb=version_nb,
     )
     logger.info(f"TEI file created at {target_filepath}")
 
